df7d794f828217b9994f1433c86336869fee3234
Bello S Babakolo <fleetbeekay@gmail.com>
2021-01-27 09:41:48 +0100

    Add shooting to player and create enemy enitity

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,26 +1,48 @@
 import Entity from './Entity';
+import PlayerLaser from './Player-laser';
 
 export default class Player extends Entity {
     constructor(scene, x, y, key) {
         super(scene, x, y, key, "Player");
         this.setData("speed", 200);
-        this.setData("rotationSpeed", 2);
+        this.setData("isShooting", false);
+        this.setData("timerShootDelay", 10);
+        this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
         this.scale = 0.2;
     }
 
-    thrust() {
-        // this.body.velocity.y = -this.getData("speed");
+    moveUp() {
+        this.body.velocity.y = -this.getData("speed");
     }
 
-    rotateClockwise() {
-        this.angle += this.getData("rotationSpeed");
+    moveDown() {
+        this.body.velocity.y = this.getData("speed");
     }
 
-    rotateAntiClockwise() {
-        this.angle -= this.getData("rotationSpeed");
+    moveLeft() {
+        this.body.velocity.x = -this.getData("speed");
+    }
+
+    moveRight() {
+        this.body.velocity.x = this.getData("speed");
     }
 
     update() {
+        this.body.setVelocity(0, 0);
 
+        this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
+        this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+        if (this.getData("isShooting")) {
+            if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+              this.setData("timerShootTick", this.getData("timerShootTick") + 1);
+            } else {
+              let laser = new PlayerLaser(this.scene, this.x, this.y);
+              this.scene.playerLasers.add(laser);
+            
+              // this.scene.sfx.laser.play();
+              this.setData("timerShootTick", 0);
+            }
+        }
     }
 }
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import backgroundImg from '../assets/space-background.png';
-import playerShip from '../assets/player-ship.png';
+import playerShipImg from '../assets/player-ship.png';
+import playerLaserImg from '../assets/sprLaserPlayer.png';
+import enemyShipImg from '../assets/enemy.png';
 import Player from '../entities/Player';
 
 export default class MainScene extends Phaser.Scene {
@@ -10,7 +12,11 @@ export default class MainScene extends Phaser.Scene {
 
     preload (){
         this.load.image('background', backgroundImg);
-        this.load.image('player-ship', playerShip);
+
+        this.load.image('player-ship', playerShipImg);
+        this.load.image("player-ship-laser", playerLaserImg);
+
+        this.load.image('enemy-ship', enemyShipImg)
     }
     
     create() {
@@ -22,18 +28,32 @@ export default class MainScene extends Phaser.Scene {
             'player-ship'
         );
 
+        this.playerLasers = this.add.group();
+
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update() {
-        if (this.cursors.left.isDown) {
-            this.player.rotateAntiClockwise();
-        } else if (this.cursors.right.isDown) {
-            this.player.rotateClockwise();
-        }
+        this.player.update();
 
         if (this.cursors.up.isDown) {
-            console.log("Thrust the JET Forward");
+            this.player.moveUp();
+        } else if (this.cursors.down.isDown) {
+            this.player.moveDown();
+        }
+
+        if (this.cursors.left.isDown) {
+            this.player.moveLeft();
+        } else if (this.cursors.right.isDown) {
+            this.player.moveRight();
+        }
+
+        if (this.keySpace.isDown) {
+            this.player.setData("isShooting", true);
+        } else {
+            this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+            this.player.setData("isShooting", false);
         }
     }
 };
