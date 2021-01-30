6847e4a89f66e9e919c861635b14f21b1c41d574
Bello S Babakolo <fleetbeekay@gmail.com>
2021-01-30 15:26:52 +0100

    Ready for deploy

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require('express');
-
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const http = require('http').createServer(app);
 
-const BASE_DIR = __dirname;
+app.use(express.static(__dirname + '/dist'));
 
-console.log(`${__dirname}/dist/index.html`);
-
-app.use(express.static(`${BASE_DIR}/dist`));
-
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-http.listen(PORT, () => {
-  console.log(`Listening on ${http.address().port}`);
-});
+app.listen(port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   output: {
