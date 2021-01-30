147fc7456bef296332cbf47764a8dee1e6f49b92
Bello S Babakolo <fleetbeekay@gmail.com>
2021-01-30 14:09:22 +0100

    Add heroku postbuild

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 
-app.use(express.static(`${__dirname}/dist`));
+const BASE_DIR = __dirname;
+
+console.log(`${__dirname}/dist/index.html`);
+
+app.use(express.static(`${BASE_DIR}/dist`));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
